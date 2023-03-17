d473476f941f067d4d3c11b1af1b24faed16af49
Erik Stafford <skyelstafford@gmail.com>
2023-03-16 21:06:24 -0400

    Create Printer Object Class
    Fixes $117 Create Printer Object

--- PrinterOB.cs
+++ PrinterOB.cs
@@ -3,11 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace PrinterServerToolbox
 {
     public class PrinterOB
     {
+        private string portIP;
+        private bool isNullPort;
+        Regex IPFormat = new Regex(@"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}");
+        Regex IPNulFormat = new Regex(@"nul");
 
+        public string Name;
+        public string Comment;
+        public string Location;
+        public string PortName;
+        public string PortIP
+        {
+            get
+            {
+                return portIP;
+            }
+            set
+            {
+                if (IPFormat.IsMatch(value))
+                {
+                    portIP = value;
+                }
+                else if (IPNulFormat.IsMatch(value))
+                {
+                    isNullPort = true;
+                    PortIP = value;
+                }
+                else
+                {
+                    portIP = "0.0.0.0";
+                }
+            }
+        }
+        public PrinterDriver Driver;
+        public bool AdvancedPrinterFeaturesEnabled;
+        public string PrintProcessor;
+        public PrinterPropertys PrinterDefaults;
     }
 }
