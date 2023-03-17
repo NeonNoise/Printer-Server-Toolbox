31f4dad218526cf4a6344c8659afa1cc6f33f783
Erik Stafford <skyelstafford@gmail.com>
2023-03-16 20:42:22 -0400

    Create Printer Propertys Class
    Fixes $118 Create Print Settings Object

---
+++ PrinterPropertys.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrinterServerToolbox
+{
+    public class PrinterPropertys
+    {
+        private string shortPath;
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Path
+        {
+            get
+            {
+                return (shortPath + name);
+            }
+        }
+    }
+}
