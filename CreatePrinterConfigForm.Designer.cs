ee537d0d3de054a70d7cc9ebfe457df78ede4e19
Erik "Skye" Stafford <skyelstafford@gmail.com>
2023-03-21 15:25:57 -0400

    Update PrinterConfigForm

--- CreatePrinterConfigForm.Designer.cs
+++ CreatePrinterConfigForm.Designer.cs
@@ -29,20 +29,20 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CreatePrinterConfigForm));
-            this.button1 = new System.Windows.Forms.Button();
+            this.RefrshPrintersbutton = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.CreateConfigbutton = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // RefrshPrintersbutton
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(267, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Refresh Printers";
-            this.button1.UseVisualStyleBackColor = true;
+            this.RefrshPrintersbutton.Location = new System.Drawing.Point(12, 12);
+            this.RefrshPrintersbutton.Name = "RefrshPrintersbutton";
+            this.RefrshPrintersbutton.Size = new System.Drawing.Size(267, 23);
+            this.RefrshPrintersbutton.TabIndex = 0;
+            this.RefrshPrintersbutton.Text = "Refresh Printers";
+            this.RefrshPrintersbutton.UseVisualStyleBackColor = true;
             // 
             // comboBox1
             // 
@@ -52,14 +52,14 @@
             this.comboBox1.Size = new System.Drawing.Size(266, 21);
             this.comboBox1.TabIndex = 1;
             // 
-            // button2
+            // CreateConfigbutton
             // 
-            this.button2.Location = new System.Drawing.Point(13, 160);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(266, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Create Config for Selected Printer";
-            this.button2.UseVisualStyleBackColor = true;
+            this.CreateConfigbutton.Location = new System.Drawing.Point(13, 160);
+            this.CreateConfigbutton.Name = "CreateConfigbutton";
+            this.CreateConfigbutton.Size = new System.Drawing.Size(266, 23);
+            this.CreateConfigbutton.TabIndex = 2;
+            this.CreateConfigbutton.Text = "Create Config for Selected Printer";
+            this.CreateConfigbutton.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
@@ -77,9 +77,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(311, 221);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.CreateConfigbutton);
             this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.RefrshPrintersbutton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "CreatePrinterConfigForm";
             this.Text = "Create Printer Config";
@@ -90,9 +90,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button RefrshPrintersbutton;
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button CreateConfigbutton;
         private System.Windows.Forms.TextBox textBox1;
     }
 }
